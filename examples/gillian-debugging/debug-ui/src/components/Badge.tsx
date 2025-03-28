61dc41104714761d2e89f0bb3972bd6c66662775
Nat Karmios <nat@karmios.com>
2025-03-28 12:29:48 +0000

    Remove Gillian VSCode extension

--- examples/gillian-debugging/debug-ui/src/components/Badge.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentOverrideProps } from "@gillianplatform/sedap-react";
-import { VSCodeBadge } from "@vscode/webview-ui-toolkit/react";
-
-const Badge: React.FC<ComponentOverrideProps> = ({ className, style, children }) => {
-  const props = { className, style, children } as const;
-  return <VSCodeBadge {...props} />;
-};
-
-export default Badge;
