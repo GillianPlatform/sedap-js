616fc95212a581ea81e402da823e2e24058137e8
Nat Karmios <nat@karmios.com>
2025-05-23 17:54:01 +0100

    Add icons to highlighted nodes

--- react/src/components/nodes/util/NodeItems.tsx
+++ react/src/components/nodes/util/NodeItems.tsx
@@ -1,32 +1,24 @@
 import { Fragment } from "react";
 import { MapNodeExtra } from "@gillianplatform/sedap-types";
 import renderExtras from "./renderExtras";
-import styled from "styled-components";
+import Row from "./Row";
 
 type NodeItemsProps = {
-  items?: Record<string, React.ReactNode>;
+  items?: [string, React.ReactNode][];
   extras?: MapNodeExtra[];
 };
 
-const NodeItemRow = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 0.25em;
-`;
-
 const NodeItems: React.FC<NodeItemsProps> = ({ items, extras }) => {
-  const filteredItems = Object.entries(items || {}).filter(([, v]) => v);
+  const filteredItems = (items || []).filter(([, v]) => v);
   const renderedExtras = renderExtras(extras || []).map((e, i) => [`extra-${i}`, e] as const);
   const allItems = [...renderedExtras, ...filteredItems];
   if (allItems.length === 0) return null;
   return (
-    <NodeItemRow>
+    <Row>
       {allItems.map(([key, item]) => (
         <Fragment key={key}>{item}</Fragment>
       ))}
-    </NodeItemRow>
+    </Row>
   );
 };
 
